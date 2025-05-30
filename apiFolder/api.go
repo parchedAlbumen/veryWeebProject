5fa802feb2124ffbcea718166b957e44ce85806c
parchedAlbumen <kennethpamintuan2305@gmail.com>
2025-05-29 17:49:40 -0700

    check statistics feature added

--- apiFolder/api.go
+++ apiFolder/api.go
@@ -16,7 +16,7 @@ type Manga struct {
 }
 
 type MangaData struct {
-	Data []Manga `json:"data"`
+	Data []Manga `json:"data"` //this is a slice of object: Manga
 }
 
 type MangaScore struct {
@@ -28,15 +28,14 @@ type MangaScore struct {
 type ScoreData struct {
 	Completed int          `json:"completed"`
 	Total     int          `json:total"`
-	Scores    []MangaScore `json:scores"`
+	Scores    []MangaScore `json:scores"` //this is a slice of object: MangaScore
 }
 
 type MangaScoreData struct {
-	Data []ScoreData `json:"data"`
+	Data ScoreData `json:"data"`
 }
 
 func GetRec(manga []Manga) string {
-	// getManga(data, name)
 	if len(manga) > 0 {
 		first := manga[0]
 		fmt.Println("giving user informations")
@@ -57,26 +56,29 @@ func GetSynopsis(data *MangaData, name string) string {
 }
 
 func GetMangaScore(data *MangaData, name string) string {
-	//get manga id first
-	//bro i love this initalization and if statement in one line
 	if id := getMangaId(data, name); id > 0 {
-		//do this
-		httpStuff := "https://api.jikan.moe/v4/manga/" + strconv.Itoa(id) + "/statistics"
+		httpreq := "https://api.jikan.moe/v4/manga/" + strconv.Itoa(id) + "/statistics"
 		var scoreData MangaScoreData
-		yomama := getScoreStatistic(httpStuff, &scoreData) //yo mama is []ScoreData
-		if len(yomama) > 0 {
-			//do magic wor here
+		dataInfo := getScoreStatistic(httpreq, &scoreData)
+		if len(dataInfo.Scores) > 0 {
+			infoFormat := ""
+			fmt.Println(infoFormat)
+			infoFormat += ("Completed: " + strconv.Itoa(dataInfo.Completed) + "\n")
+			infoFormat += ("Total: " + strconv.Itoa(dataInfo.Total) + "\n")
+			mangaScores := dataInfo.Scores[0]
+			infoFormat += ("Votes: " + strconv.Itoa(mangaScores.Votes) + "\n")
+			infoFormat += ("Percentages: " + strconv.Itoa(mangaScores.Percentage) + "%\n")
+			return infoFormat
 		} else {
-			return "bad at yomama"
+			return "no info what the!"
 		}
-		return "hi"
 	} else {
 		return "bad at getMangaScore"
 	}
 }
 
-// why do I thik []scoredata is the right return type, figure this out
-func getScoreStatistic(theReq string, data *MangaScoreData) []ScoreData {
+func getScoreStatistic(theReq string, data *MangaScoreData) ScoreData {
+	fmt.Println(theReq + "lol")
 	resp, err := http.Get(theReq)
 	if err != nil {
 		fmt.Println(err)
@@ -87,7 +89,6 @@ func getScoreStatistic(theReq string, data *MangaScoreData) []ScoreData {
 	if err != nil {
 		fmt.Println(err)
 	}
-
 	json.Unmarshal(body, &data)
 	return data.Data
 }
@@ -114,8 +115,6 @@ func getManga(data *MangaData, name string) []Manga {
 	if err != nil {
 		fmt.Println(err)
 	}
-
 	json.Unmarshal(body, &data)
-
 	return data.Data
 }
--- gui/hello.py
+++ gui/hello.py
@@ -21,9 +21,15 @@ def getSynopsis():
     dataName = {"mangaName": manga_var.get()}
 
     response = requests.post("http://localhost:3333/skibidiRizzlerSigmaMale", json=dataName)
-    print("Response from the lebron server:", response.text)
     manga_var.set("")
     msg.config(text=response.text)
+
+def getScore():
+    dataName = {"mangaName": manga_var.get()}
+
+    response = requests.post("http://localhost:3333/getScore", json=dataName)
+    msg.config(text=response.text)
+    manga_var.set("")
 
 def getRecommendation():
     mangaName = manga_var.get()
@@ -42,7 +48,7 @@ manga_name =  tkinter.Label(root, text="Name of the Manga:")
 manga_entry = tkinter.Entry(root, textvariable=manga_var)
 
 #the buttons of what I can do 
-get_rec_button = tkinter.Button(root, text="Get Recommendation", command=getRecommendation)
+get_score= tkinter.Button(root, text="Get Score", command=getScore)
 get_synopsis = tkinter.Button(root, text="Get Synopsis", command=getSynopsis)
 
 #make the message thing here
@@ -51,7 +57,7 @@ msg = tkinter.Label(root, text=init_msg, wraplength=300)
 #the grid stuff, basically how its set up
 manga_name.grid(row=0,column=0)
 manga_entry.grid(row=0,column=1)
-get_rec_button.grid(row=1,column=0)
+get_score.grid(row=1,column=0)
 get_synopsis.grid(row=1,column=1)
 msg.grid(row=0, column=3,padx=100)
 
